496c02a3e2cd278885ae73cd300f11c59fb66eba
Adam Li <adam2392@gmail.com>
2017-05-26 13:40:33 -0700

    Trying to push content.

--- publishconf.py
+++ publishconf.py
@@ -18,25 +18,27 @@ CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
-PLUGIN_PATHS = ['./plugins']
-PLUGINS = ['extract_toc','render_math','disqus_static','better_figures_and_images']
-MD_EXTENSIONS = ['codehilite','extra','smarty', 'toc']
+# MARKUP = ('rst', 'md', 'html')
+
+# PLUGIN_PATHS = ['./plugins']
+# PLUGINS = ['extract_toc','render_math','disqus_static','better_figures_and_images']
+# MD_EXTENSIONS = ['codehilite','extra','smarty', 'toc']
 
 # Set Directives
-PATH = '../raw'
-OUTPUT_PATH = '../www.notionsandnotes.org/'
-STATIC_PATHS = ['extra', 'images', 'pdfs']
-EXTRA_PATH_METADATA = {
-    'extra/robots.txt': {'path': 'robots.txt'},
-    'extra/favicon.ico': {'path': 'favicon.ico'},
-    'extra/htaccess': {'path': '.htaccess'}
-}
+# PATH = '/content/'
+# OUTPUT_PATH = '../www.notionsandnotes.org/'
+# STATIC_PATHS = ['extra', 'images', 'pdfs']
+# EXTRA_PATH_METADATA = {
+#     'extra/robots.txt': {'path': 'robots.txt'},
+#     'extra/favicon.ico': {'path': 'favicon.ico'},
+#     'extra/htaccess': {'path': '.htaccess'}
+# }
 
-THEME = 'themes/built-texts'
+# THEME = 'themes/built-texts'
 
-COLOPHON = True
-COLOPHON_TITLE = 'About'
-COLOPHON_CONTENT = "Mainly...."
+# COLOPHON = True
+# COLOPHON_TITLE = 'About'
+# COLOPHON_CONTENT = "Mainly...."
 
 # Following items are often useful when publishing
 
