160c70f9e068f16ebcd09e409f2419c46336ebe3
Adam Li <adam2392@gmail.com>
2017-05-27 20:14:27 -0700

    Pushing up changes to theme.

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,7 +8,7 @@ SITEURL = ''
 
 PATH = 'content'
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'America/New_York'
 
 DEFAULT_LANG = u'en'
 
@@ -19,15 +19,35 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+# to run the theme 
+# for now, pelican-bootstrap3
+# THEME = 'pelican-themes/pelican-bootstrap3'
+# JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+# PLUGIN_PATHS = ['/path/to/git/pelican-plugins'] 
+# PLUGINS = ['i18n_subsites']
+THEME = "pelican-themes/Flex/"
+
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
          ('You can modify those links in your config file', '#'),)
 
+# Main page
+MAIN_MENU = True
+DISPLAY_PAGES_ON_MENU = True
+
+MENUITEMS = (("Blog", "/blog.html"),
+             ("Archives", "/archives.html"),
+             ("Categories", "/categories.html"),
+             ("Tags", "/tags.html"),)
+
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+GITHUB_URL = "https://github.com/adam2392"
+TWITTER_USERNAME = "adam2392"
+SOCIAL = (("twitter", "https://twitter.com/adam2392"),
+          ("github", "https://github.com/adam2392"),
+          ("linkedin", "https://www.linkedin.com/in/adam2392"))
 
 DEFAULT_PAGINATION = 10
 
