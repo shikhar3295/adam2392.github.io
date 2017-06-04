65cd582ab85bcd4d1fc4022173fa7f70812993b5
Adam Li <adam2392@gmail.com>
2017-06-04 00:20:05 -0400

    Adding setting up of pelican site and its tags.

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,13 +5,28 @@ from __future__ import unicode_literals
 # jinja2.Environment.install_gettext_callables
 
 AUTHOR = u'Adam Li'
-SITENAME = u"Adam Li's Website"
+SITENAME = u"Adam Li"
 SITEURL = 'http://localhost:8000'
 # SITELOGO = 
 SITETITLE = 'Adam J. Li'
 COPYRIGHT_YEAR = 2017
 
 PATH = 'content'
+
+# Photo blog settings | https://github.com/getpelican/pelican-plugins/tree/master/photos
+PHOTO_LIBRARY = 'photos'
+PHOTO_GALLERY = (1024, 768, 80)	# For photos in galleries, maximum width and height, plus JPEG quality
+PHOTO_ARTICLE = (760, 506, 80)	# For photos associated with articles, maximum width, height, and quality
+PHOTO_THUMB = (192, 144, 60)	# For thumbnails, maximum width, height, and quality
+PHOTO_RESIZE_JOBS = 5 			# Number of parallel resize jobs to be run.
+# PHOTO_WATERMARK = True # Adds a watermark to all photos in articles and pages. Defaults to using your site name.
+# PHOTO_WATERMARK_TEXT = 'ADAM LI' # Allow the user to change the watermark text or remove it completel
+
+PHOTO_EXIF_KEEP = True
+PHOTO_EXIF_REMOVE_GPS = True  # Removes any GPS information from t
+PHOTO_EXIF_COPYRIGHT = 'COPYRIGHT'
+PHOTO_EXIF_COPYRIGHT_AUTHOR = 'Adam Li'
+
 
 TIMEZONE = 'America/New_York'
 DEFAULT_LANG = u'en'
@@ -33,24 +48,25 @@ JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
 # THEME = "pelican-themes/Flex/"
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
 
 # Main page
-# MAIN_MENU = True
-# DISPLAY_PAGES_ON_MENU = True
+MAIN_MENU = True
+DISPLAY_PAGES_ON_MENU = True
 
-# MENUITEMS = (("Blog", "/blog.html"),
-#              ("Archives", "/archives.html"),
-#              ("Categories", "/categories.html"),
-#              ("Tags", "/tags.html"),)
+MENUITEMS = (("Blog", "/blog.html"),
+             ("Archives", "/archives.html"),
+             ("Categories", "/categories.html"),
+             ("Tags", "/tags.html"),)
 
 # Social widget
 GITHUB_URL = "https://github.com/adam2392"
 TWITTER_USERNAME = "adam2392"
 SOCIAL = (("twitter", "https://twitter.com/adam2392"),
+		  ("stack-overflow", "https://stackexchange.com/users/4494355/ajl123"),
           ("github", "https://github.com/adam2392"),
           ("linkedin", "https://www.linkedin.com/in/adam2392"))
 
