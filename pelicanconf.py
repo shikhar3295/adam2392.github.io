8b9dbcae5a88c7c22bd33b8b9dad88c752c4b170
Adam Li <adam2392@gmail.com>
2017-06-04 17:29:27 -0400

    Testing list of recent articles.

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,19 +14,19 @@ HEADER_IMAGE='header.JPG'
 
 PATH = 'content'
 
-# Photo blog settings | https://github.com/getpelican/pelican-plugins/tree/master/photos
-PHOTO_LIBRARY = 'photos'
-PHOTO_GALLERY = (1024, 768, 80)	# For photos in galleries, maximum width and height, plus JPEG quality
-PHOTO_ARTICLE = (760, 506, 80)	# For photos associated with articles, maximum width, height, and quality
-PHOTO_THUMB = (192, 144, 60)	# For thumbnails, maximum width, height, and quality
-PHOTO_RESIZE_JOBS = 5 			# Number of parallel resize jobs to be run.
-# PHOTO_WATERMARK = True # Adds a watermark to all photos in articles and pages. Defaults to using your site name.
-# PHOTO_WATERMARK_TEXT = 'ADAM LI' # Allow the user to change the watermark text or remove it completel
+# # Photo blog settings | https://github.com/getpelican/pelican-plugins/tree/master/photos
+# PHOTO_LIBRARY = 'photos'
+# PHOTO_GALLERY = (1024, 768, 80)	# For photos in galleries, maximum width and height, plus JPEG quality
+# PHOTO_ARTICLE = (760, 506, 80)	# For photos associated with articles, maximum width, height, and quality
+# PHOTO_THUMB = (192, 144, 60)	# For thumbnails, maximum width, height, and quality
+# PHOTO_RESIZE_JOBS = 5 			# Number of parallel resize jobs to be run.
+# # PHOTO_WATERMARK = True # Adds a watermark to all photos in articles and pages. Defaults to using your site name.
+# # PHOTO_WATERMARK_TEXT = 'ADAM LI' # Allow the user to change the watermark text or remove it completel
 
-PHOTO_EXIF_KEEP = True
-PHOTO_EXIF_REMOVE_GPS = True  # Removes any GPS information from t
-PHOTO_EXIF_COPYRIGHT = 'COPYRIGHT'
-PHOTO_EXIF_COPYRIGHT_AUTHOR = 'Adam Li'
+# PHOTO_EXIF_KEEP = True
+# PHOTO_EXIF_REMOVE_GPS = True  # Removes any GPS information from t
+# PHOTO_EXIF_COPYRIGHT = 'COPYRIGHT'
+# PHOTO_EXIF_COPYRIGHT_AUTHOR = 'Adam Li'
 
 # Handling Articles
 PAGE_URL = "{slug}/"
@@ -34,6 +34,7 @@ PAGE_SAVE_AS = "{slug}/index.html"
 ARTICLE_URL = "blog/{date:%Y}/{date:%m}/{slug}/"
 ARTICLE_SAVE_AS = "blog/{date:%Y}/{date:%m}/{slug}/index.html"
 
+RELATED_POSTS_MAX = 10
 
 TIMEZONE = 'America/New_York'
 DEFAULT_LANG = u'en'
@@ -48,9 +49,8 @@ AUTHOR_FEED_RSS = None
 # to run the theme | for now, pelican-bootstrap3
 THEME = 'pelican-themes/pelican-bootstrap3'
 PLUGIN_PATHS = ['plugins'] 
-PLUGINS = ['i18n_subsites', 'render_math']
+PLUGINS = ['i18n_subsites', 'render_math', 'related_posts']
 JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
-# JINJA_EXTENSIONS = {'extensions': ['jinja2.ext.i18n']}
 # THEME = "pelican-themes/Flex/"
 
 # Blogroll
@@ -63,13 +63,14 @@ JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
 MAIN_MENU = True
 DISPLAY_PAGES_ON_MENU = True
 
-MENUITEMS = (("Blog", "/blog.html"),
+MENUITEMS = (("Blog", "/article_list.html"),
              ("Archives", "/archives.html"),
              ("Categories", "/categories.html"),
              ("Tags", "/tags.html"),)
 
 # Social widget
 GITHUB_URL = "https://github.com/adam2392"
+GITHUB_USER = 'adam2392'
 TWITTER_USERNAME = "adam2392"
 SOCIAL = (("twitter", "https://twitter.com/adam2392"),
 		  ("stack-overflow", "https://stackexchange.com/users/4494355/ajl123"),
